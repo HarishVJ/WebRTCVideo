2a33449c9dd989e0245dad45e561415a694471b5
harishkumar.vj@riverway.in <harishkumar.vj@riverway.in>
2021-03-01 23:50:12 -0500

    "[Updated with the slider implementation]"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import debounce from "lodash/debounce";
 declare var $;
 
+const currentObj = this;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,42 +13,55 @@ declare var $;
 
 
 export class AppComponent implements AfterViewInit {
+  @ViewChild('divContainer', { static: true }) gallery: ElementRef;
 
-
-
+  private videoCount: number = 9;
+  private videoAlignCount: number = 9;
+  private containerName: string = "";
+  private screenWidth: number = 0;
+  private screenHeight: number = 0;
   constructor(
     private elementRef: ElementRef
-  ) { }
-
-  @ViewChild('gallery', { static: true }) gallery: ElementRef;
+  ) {
 
 
+
+  }
   title = 'riverwayVideo';
 
   ngOnInit() {
+    this.screenWidth = document.body.getBoundingClientRect().width;
+    this.screenHeight = document.body.getBoundingClientRect().height;
+    let videoCnt = this.videoCount > this.videoAlignCount ? this.videoAlignCount : this.videoCount;
 
-
+    document.getElementById("temp").innerHTML = "divContainer1" + "," + videoCnt + "," + this.screenWidth + "," + this.screenHeight;
+    this.addVideoControls(this.videoCount);
   }
 
   ngAfterViewInit() {
 
-    // console.log(this.gallery);
-    // this.gallery.nativeElement.innerHTML = "Hello Angular 10!";
-    const debouncedRecalculateLayout = debounce(this.recalculateLayout, 50);
-    window.addEventListener("resize", debouncedRecalculateLayout);
-    debouncedRecalculateLayout();
+    this.calculateConstraint();
 
   }
 
+  private calculateConstraint() {
+    const debouncedRecalculateLayout = debounce(this.recalculateLayout, 50);
+    window.addEventListener("resize", debouncedRecalculateLayout);
+    debouncedRecalculateLayout();
+  }
 
 
+  private recalculateLayout() {
 
-  recalculateLayout() {
-    const gallery = document.getElementById("gallery");
-    const aspectRatio = 16 / 9;
-    const screenWidth = document.body.getBoundingClientRect().width;
-    const screenHeight = document.body.getBoundingClientRect().height;
-    const videoCount = document.getElementsByTagName("video").length;
+    let tempvalue = document.getElementById("temp").innerHTML;
+    let containerName = tempvalue.split(',')[0];
+    let videoCount = parseInt(tempvalue.split(',')[1]);
+    let gallery = document.getElementById(containerName);
+    let aspectRatio = 16 / 9;
+    // document.getElementById("divContainer1").getBoundingClientRect().width = document.body.getBoundingClientRect().width;
+    // document.getElementById("divContainer1").getBoundingClientRect().height = document.body.getBoundingClientRect().height;
+    let screenWidth = parseFloat(tempvalue.split(',')[2]);
+    let screenHeight = parseFloat(tempvalue.split(',')[3]);
 
     // or use this nice lib: https://github.com/fzembow/rect-scaler
 
@@ -58,21 +73,120 @@ export class AppComponent implements AfterViewInit {
       aspectRatio
     );
 
-    // $(".video-container").attr("width", layout.width + "px");
-    // $(".video-container").attr("height", layout.height + "px");
-    // $(".video-container").attr("cols", layout.cols + "px");
-    this.gallery["style"].setProperty("--width", layout.width + "px");
-    this.gallery["style"].setProperty("--height", layout.height + "px");
-    this.gallery["style"].setProperty("--cols", layout.cols + "");
+    gallery["style"].setProperty("--width", layout.width + "px");
+    gallery["style"].setProperty("--height", layout.height + "px");
+    gallery["style"].setProperty("--cols", layout.cols + "");
   }
 
-  deleteFrame(index) {
+
+  private addVideoControls(videoCount) {
+
+    try {
+      const gallery = document.getElementById("divVideoContainer");
+      let containerIndex = 0;
+      let isReadytoAppend: boolean = false;
+      let container: any;
+      let sliderRight: any;
+      let sliderLeft: any;
+      for (let index = 0; index < videoCount; index++) {
+
+        // Creating video element 
+        var vidElement = document.createElement('video');
+        // vidElement.setAttribute('autoplay', '');
+        vidElement.setAttribute('muted', "true");
+        vidElement.src = "../assets/rr.mp4";
+
+        //Creating video container div
+
+        var vidContainer = document.createElement('div');
+        vidContainer.setAttribute('id', 'video' + index);
+        vidContainer.setAttribute('class', 'video-container');
+        vidContainer.appendChild(vidElement);
+        vidContainer.appendChild(this.makeLabel("user" + index));
+        let currentObect = this;
+        vidContainer.addEventListener("click", function () {
+          currentObect.deleteFrame(index)
+        });
+
+        if (index == 0 || (index >= this.videoAlignCount && index % this.videoAlignCount === 0)) {
+          containerIndex = containerIndex + 1
+          isReadytoAppend = true;
+          container = document.createElement('div');
+          container.setAttribute('id', 'divContainer' + containerIndex);
+          container.setAttribute('class', 'container');
+          sliderRight = document.createElement('div');
+          sliderRight.setAttribute('id', 'sliderRight' + containerIndex);
+          sliderRight.setAttribute('class', 'divsliderRight');
+          // sliderRight.html('>');
+          sliderRight.addEventListener("click", function () {
+            currentObect.toggleContainer(containerIndex, videoCount)
+          });
+
+          sliderLeft = document.createElement('div');
+          sliderLeft.setAttribute('id', 'sliderLeft' + containerIndex);
+          sliderLeft.setAttribute('class', 'divsliderLeft');
+          sliderLeft.addEventListener("click", function () {
+            currentObect.toggleContainer(containerIndex -1, videoCount)
+          });
+          if (index != 0) {
+            container["style"].setProperty("display", "none");
+            sliderRight["style"].setProperty("display", "none");
+            sliderLeft["style"].setProperty("display", "none");
+
+          }
+        }
+        container.appendChild(vidContainer);
+
+        if (isReadytoAppend) {
+          gallery.appendChild(container);
+          gallery.appendChild(sliderRight);
+          gallery.appendChild(sliderLeft);
+        }
+
+
+
+      }
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  toggleContainer(containerIndex: number, videoCount: any) {
+
+    let getContainerCount = Math.ceil(videoCount / this.videoAlignCount);
+    for (let index = 1; index <= containerIndex; index++) {
+
+      if (index != containerIndex)
+        document.getElementById("divContainer" + index)["style"].setProperty("display", "none");
+      // $('#divContainer' + index).remove();
+    }
+    // document.getElementById("divContainer" + containerIndex)["style"].setProperty("display", "block");
+    document.getElementById("divContainer" + containerIndex)["style"].setProperty("display", "flex");
+    document.getElementById("temp").innerHTML = "divContainer" + containerIndex + "," + (this.videoCount - this.videoAlignCount) + "," + this.screenWidth + "," + this.screenHeight;
+    this.calculateConstraint();
+
+
+
+  }
+
+  private deleteFrame(index) {
 
     // debugger;
     $('#video' + index).remove();
     this.ngAfterViewInit();
 
   }
+
+
+  private makeLabel(label) {
+    var vidLabel = document.createElement('div');
+    vidLabel.appendChild(document.createTextNode(label));
+    vidLabel.setAttribute('class', 'videoLabel');
+    return vidLabel;
+  }
+
+
 
 
 
@@ -107,7 +221,7 @@ function calculateLayout(
       height = Math.floor(containerHeight / rows);
       width = Math.floor(height * aspectRatio);
     }
-    const area = width * height;
+    let area = width * height;
     if (area > bestLayout.area) {
       bestLayout = {
         area,
